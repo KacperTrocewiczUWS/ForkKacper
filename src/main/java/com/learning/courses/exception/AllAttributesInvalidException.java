bf890d1f0d5deda3ca5123e0655e454e66b95507
KacperTrocewiczUWS <kt88639@stud.uws.edu.pl>
2025-05-11 19:20:51 +0200

    Creating file upload functionallity

---
+++ src/main/java/com/learning/courses/exception/AllAttributesInvalidException.java
@@ -0,0 +1,7 @@
+package com.learning.courses.exception;
+
+import java.io.IOException;
+
+public class AllAttributesInvalidException extends IOException {
+    public AllAttributesInvalidException(String message) { super(message); }
+}
